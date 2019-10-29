d6f6afb61eb6107e9c3b45b5d0e17df802e896b8
Akshay Alenchery <akshayalenchery@gmail.com>
2019-10-29 08:59:23 +0530

    Added first 2 chapters excercise

--- 002.js
+++ 002.js
@@ -3,7 +3,7 @@
 const fizzbizz = () => {
   for (let i = 1; i <= 100; i++) {
     if (i % 3 === 0 && i % 5 === 0) console.log('FizzBizz')
-    else if (i % 3 === 0) console.log('Fizz')
+    if (i % 3 === 0) console.log('Fizz')
     else if (i % 5 === 0) console.log('Bizz')
     else console.log(i)
   }
